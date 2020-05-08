7d9b7a67806e32e52eb9cae831c6c02f56bd37fc
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-05-08 13:26:45 +0530

    Create 24.Bon appetit.cpp

---
+++ 24.Bon appetit.cpp
@@ -0,0 +1,30 @@
+"""
+Link of the question...
+https://www.hackerrank.com/challenges/bon-appetit/problem
+Solution...
+"""
+#include<iostream>
+using namespace std;
+int main()
+{
+int sum=0,n,k,*bill,i,num;
+cin>>n;
+cin>>k;
+bill= new int [n];
+for(i=0;i<n;i++)
+cin>>*(bill+i);
+cin>>num;
+for(i=0;i<n;i++)
+{
+    if(k!=i)
+    sum=sum+*(bill+i);
+}
+sum=sum/2;
+if(sum==num)
+cout<<"Bon Appetit";
+else 
+cout<<num-sum;
+return 0;
+
+
+}
