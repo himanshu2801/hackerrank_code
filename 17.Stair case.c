1e00373d9f155518c59180d2496ce967a7b91242
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-05-08 13:13:22 +0530

    Create 17.Stair case.c

---
+++ 17.Stair case.c
@@ -0,0 +1,24 @@
+"""
+Link of the question...
+hackerrank.com/challenges/staircase
+Solution...
+"""
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {   
+        for(j=0;j<n;j++)
+        {
+            if(j<n-i-1)
+            printf(" ");
+            else
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
