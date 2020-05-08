17c39f1c7ff8a61e6579de0163f011bb9405dfbf
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-05-08 13:05:41 +0530

    Create 13.triangle quest 2.py

---
+++ 13.triangle quest 2.py
@@ -0,0 +1,12 @@
+"""
+Link of the Question...
+https://www.hackerrank.com/challenges/triangle-quest-2/problem
+Solution...
+"""
+
+
+
+
+for i in range(1,int(input())+1):
+    print(((10**i)//9)*((10**i)//9))
+
