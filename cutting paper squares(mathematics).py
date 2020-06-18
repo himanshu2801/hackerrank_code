4cc26490db64e15e3e4fe364bbac30070827fc8b
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-06-18 18:24:09 +0530

    Create cutting paper squares(mathematics).py

---
+++ cutting paper squares(mathematics).py
@@ -0,0 +1,32 @@
+"""
+link of the question...
+https://www.hackerrank.com/challenges/p1-paper-cutting/problem
+solution...
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(n, m):
+    return n*m-1
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    result = solve(n, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
