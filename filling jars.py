fc71402a486832c2aedc01a352e1d5c86cdfd026
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-07-09 17:19:23 +0530

    Create filling jars.py

---
+++ filling jars.py
@@ -0,0 +1,38 @@
+"""
+Link of the question...
+https://www.hackerrank.com/challenges/filling-jars/problem
+Solution...
+"""
+#!/bin/python3
+
+import os
+import sys
+
+# Complete the solve function below.
+def solve(n, operations):
+    sum=0
+    for i in range(len(operations)):
+        sum=sum + (operations[i][1] - operations[i][0] + 1)*operations[i][2]
+    avr=sum//n
+    return avr
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    operations = []
+
+    for _ in range(m):
+        operations.append(list(map(int, input().rstrip().split())))
+
+    result = solve(n, operations)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
