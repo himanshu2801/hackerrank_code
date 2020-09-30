a200a3b3691bdf28f50835d66c4b0a6f13f87dcf
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-09-30 15:38:16 +0530

    Create electronic shop.cpp

---
+++ electronic shop.cpp
@@ -0,0 +1,28 @@
+"""
+https://www.hackerrank.com/challenges/electronics-shop/problem
+"""
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b,n,m;
+    cin>>b>>n>>m;
+    int a[n],c[m];
+    int i,j;
+    for(i=0;i<n;i++)
+    cin>>a[i];
+    for(i=0;i<m;i++)
+    cin>>c[i];
+    int max=0;
+    for(i=0;i<n;i++)
+    for(j=0;j<m;j++)
+    {
+        int temp=a[i]+c[j];
+        if(temp>=max && temp<=b)
+        max=temp;
+    }
+    if(max==0)
+    cout<<"-1";
+    else
+    cout<<max;
+}
