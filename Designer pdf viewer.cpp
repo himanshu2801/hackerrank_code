184d7576b5ad5982cac2d87808974229a6f752d1
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-09-30 15:41:31 +0530

    Create Designer pdf viewer.cpp

---
+++ Designer pdf viewer.cpp
@@ -0,0 +1,24 @@
+"""
+https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+"""
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a[42];
+char s[1231212];
+
+int main() {
+  for (int i = 0; i < 26; i++) {
+    scanf("%d", a+i);
+  }
+  scanf("%s", s);
+  int h = 0;
+  int w = 0;
+  for (int i = 0; s[i]!='\0'; i++) {
+    w++;
+    h = max(h, a[s[i] - 'a']);
+  }
+  printf("%d\n", h * w);
+  return 0;
+}
