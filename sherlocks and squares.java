2826e072b5d86cbb145411ff35745fb2c9ce6184
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-10-13 15:20:40 +0530

    Create sherlocks and squares.java

---
+++ sherlocks and squares.java
@@ -0,0 +1,29 @@
+"""
+https://www.hackerrank.com/challenges/sherlock-and-squares/
+"""
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+int t=sc.nextInt();
+for(int i=0;i<t;i++)
+{
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int count = (int)(Math.floor(Math.sqrt(b))- Math.ceil(Math.sqrt(a)))+1;
+    System.out.println(count);
+}
+
+    }
+}
