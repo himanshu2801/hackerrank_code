b1f01ced4f3ff4e86a1d45cdefe7f780b84f9900
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-10-13 15:15:11 +0530

    Create Save the prisoners!.java

---
+++ Save the prisoners!.java
@@ -0,0 +1,30 @@
+"""
+https://www.hackerrank.com/challenges/save-the-prisoner/problem
+"""
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    private static final Scanner s = new Scanner(System.in); 
+
+    public static void main(String[] args)
+    {
+        int t=s.nextInt();
+        for(int i=0; i<t; i++){
+            int p = s.nextInt();
+            int m = s.nextInt();
+            int id = s.nextInt();
+            if((m+id-1)%p==0)
+            System.out.println(p);
+            else 
+            System.out.println((m+id-1)%p);
+        }
+    }
+}
