5235ac7c9daffd1242cbb03992373e8f868c9913
Himanshu_Gupta <62868816+himanshu2801@users.noreply.github.com>
2020-10-13 15:18:02 +0530

    Create Jumping on the Clouds: Revisited.cpp

---
+++ Jumping on the Clouds: Revisited.cpp
@@ -0,0 +1,26 @@
+"""
+https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+"""
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k,e=100;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    for(int i=0;i<10000;i=i+k)
+    {
+        int n1=(i+k)%n;
+        if(n1!=0)
+        e=e-1-2*a[n1];
+        else
+        {
+        e=e-1-2*a[n1];
+        break;
+        }
+    }
+    cout<<e;
+}
